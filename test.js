d71148f85e880943a7958366a612b6834fe0a7df
Kevin Lindsey <kevin@kevlindev.com>
2017-06-04 19:02:17 -0700

    Return the block type and data for a given position

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 let path = require('path'),
     Map = require('./index'),
-    World = Map.World;
+    World = Map.World,
+    printTag = require('kld-nbt').printTag;
 
 let home = process.env["HOME"];
 let saves = path.join(home, "Library", "Application Support", "minecraft", "saves");
@@ -9,8 +10,12 @@ let worldName = "First World";
 let worldPath = path.join(saves, worldName);
 
 let world = new World(worldPath);
-let position = { x: 119, y: 63, z: 118 };
+let position = { x: 119, y: 62, z: 118 };
 let region = world.getRegion(position);
 let chunk = region.getChunkContainingPosition(position);
 
-console.log(chunk);
+// printTag(chunk.data);
+
+let blockType = chunk.getBlockType(position);
+
+console.log(blockType);
