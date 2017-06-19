0c892bc05a8ce240218dbc39b569688ba589a553
Kevin Lindsey <kevin@kevlindev.com>
2017-06-18 22:57:13 -0700

    Create simple block info query class
    And use it in the find_blocks.js example script

--- examples/find_blocks.js
+++ examples/find_blocks.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 let path = require('path'),
     Map = require('../index'),
-    World = Map.World;
+    World = Map.World,
+    BlockInfo = Map.BlockInfo;
 
 // build "saves" directory for macOS user
 let saves = path.join(process.env["HOME"], "Library", "Application Support", "minecraft", "saves");
 let worldName = "Forge Lucky Block";
 let world = new World(path.join(saves, worldName));
+let blockInfo = BlockInfo.findDescription("Gold Block");
 
 world.getRegions().forEach(region => {
     console.log(region.xz);
 
     region.getChunks().forEach(chunk => {
         chunk.getBlockInfos().forEach(blockType => {
-            if (blockType.type === 41) {
+            if (blockType.type === blockInfo.type && blockType.data === blockInfo.data) {
                 console.log(JSON.stringify(blockType));
             }
         })
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    BlockInfo: require('./lib/BlockInfo'),
     World: require('./lib/World'),
     Region: require('./lib/Region'),
     Chunk: require('./lib/Chunk')
